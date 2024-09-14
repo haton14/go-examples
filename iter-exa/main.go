d7a030b18c91576b737a4cb5614df3e0b516e894
haton14 <haton14@haton14.com>
2024-09-15 01:13:37 +0900

    chore: Refactor main.go file in iter-exa package

--- iter-exa/main.go
+++ iter-exa/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"iter"
+)
 
 // $ go run .
 // unko
@@ -10,6 +13,13 @@ func main() {
 	for range f {
 		fmt.Println("unko unko")
 	}
+
+	for i := range getList([]int{4, 2, 32, 44, 5}) {
+		fmt.Println(i)
+		if i == 44 {
+			break
+		}
+	}
 }
 
 func f(yield func() bool) {
@@ -18,4 +28,14 @@ func f(yield func() bool) {
 	// loopが一回終わるとyiled()の戻り値boolにloopを続行するか否かの結果を返した上で、fに戻る
 	fmt.Println(yield())
 	// yiledが呼ばれずに関数fが終了すると、loopも終了する
+}
+
+func getList(list []int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for _, v := range list {
+			if !yield(v) {
+				return
+			}
+		}
+	}
 }
